5e6932bd1d7440efa03ea65ef0c5d2265f8baf57
DUMMYONFIRE <89653048+DUMMYONFIRE@users.noreply.github.com>
2024-01-27 23:58:37 +0530

    Update face_swapper.py

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -22,7 +22,7 @@ def get_face_swapper() -> Any:
     with THREAD_LOCK:
         if FACE_SWAPPER is None:
             model_path = resolve_relative_path('../models/inswapper_128.onnx')
-            FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=['CUDAExecutionProvider'])
+            FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=['CUDAExecutionProvider']
 
     return FACE_SWAPPER
 
